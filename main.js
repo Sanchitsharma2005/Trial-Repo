1c30d63eed302b7f5d9336ed6e6e56558290ea44
Sanchitsharma2005 <85818212+Sanchitsharma2005@users.noreply.github.com>
2022-12-01 19:47:17 +0530

    Hello

---
+++ main.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal ({
+	distance: '45px',
+	duration: 2700,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:350, origin:'left'})
+sr.reveal('.home-img',{delay:350, origin:'right'})
+
+sr.reveal('.Home,.about,.team',{delay:200, origin:'bottom'})
